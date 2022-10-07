ba85e0249b6401753ad7ceaa48b9dddf76f07110
mateusz <gmateusz92@gmail.com>
2022-10-07 16:06:20 +0200

    1

--- booking/urls.py
+++ booking/urls.py
@@ -18,16 +18,17 @@ path('rooms/<uuid:room_id>/booking/payment/', views.payment, name='payment'),
 path('', views.homepage, name='homepage'),
 path('about/', views.about, name='about'),
 path('dashboard', views.dashboard, name='dashboard'),
-path('admin', views.admin_list, name='admin-list'),
-path('admin/create', users_views.admin_create, name='register'),
+#path('admin', views.admin_list, name='admin-list'),
+#path('admin/create', users_views.admin_create, name='register'),
 path('profile/', users_views.profile, name='profile'),
 path('create', views.admin_create, name='admin_create'),
-path('/admin/<uuid:staff_id>', views.show_admin, name= 'admin-staff'),
-path('/admin/<uuid:staff_id>/edit', views.edit_admin, name= 'admin-staff-edit'),
-path('/admin/<uuid:staff_id>/delete', views.delete_admin, name='admin-staff-delete'),
+#path('admin/<uuid:staff_id>', views.show_admin, name= 'admin-staff'),
+#path('admin/<uuid:staff_id>/edit', views.edit_admin, name= 'admin-staff-edit'),
+#path('admin/<uuid:staff_id>/delete', views.delete_admin, name='admin-staff-delete'),
 #path('login', auth_views.LoginView.as_view(template_name = 'hotel/login.html'), name= 'admin-login'),
 #path('logout', auth_views.LogoutView.as_view(template_name= 'hotel/logout.html'), name= 'admin-logout'),
 path('logoutuser', views.logoutuser, name='logoutuser'),
 # addition
-path('/admin/logs',views.logs, name = 'logs')
+path(''
+     'admin/logs',views.logs, name = 'logs')
 ]
--- booking/views.py
+++ booking/views.py
@@ -7,10 +7,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django import forms
 from .models import User, Receptionist, Room, RoomStatus, RoomType
-from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.db import IntegrityError
 
 def homepage(request):
-    return render(request,'hotel/index.html')
+    return render(request,'home.html')
 
 def about(request):
     return render(request,'hotel/about.html')
@@ -36,7 +37,7 @@ def payment(request,room_id):
     room_detail = get_object_or_404(RoomType, pk=room_id)
     return render(request, 'hotel/payment.html',{'room': room_detail,'rooms': room_types,'room_no':room})
 
-# authorization and authentication
+#authorization and authentication
 def admin_create(request):
     if request.method == "POST":
        form = UserRegisterForm(request.POST)
@@ -44,22 +45,38 @@ def admin_create(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, "Your account has been created successfully! You are now able to log in" + username)
-            return redirect('admin-login')
+            return redirect('home')
     else:
         form = UserRegisterForm()
-    return render(request, 'hotel/index.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
+#
+# def register_receptionist(request):
+#     if request.method == "POST":
+#        form = ReceptionistRegisterForm(request.POST)
+#        if form.is_valid():
+#           form.save()
+#           first_name = form.cleaned_data.get('first_name')
+#           messages.success(request, "Your account has been created successfully as a Receptionist! You are now able to log in" + first_name)
+#           return redirect('admin-login')
+#     else:
+#         form = ReceptionistRegisterForm()
+#     return render(request, 'hotel/register_Reception.html', {'form': form})
 
-def register_receptionist(request):
-    if request.method == "POST":
-       form = ReceptionistRegisterForm(request.POST)
-       if form.is_valid():
-          form.save()
-          first_name = form.cleaned_data.get('first_name')
-          messages.success(request, "Your account has been created successfully as a Receptionist! You are now able to log in")
-          return redirect('admin-login')
-    else:
-        form = ReceptionistRegisterForm()
-    return render(request, 'hotel/register_Reception.html', {'form': form})
+# def admin_create(request):
+#     if request.method == 'GET':
+#         return render(request, 'admin_create.html', {'form': UserCreationForm()})
+#     else:
+#         if request.POST['password1'] == request.POST['password2']:
+#             try:
+#                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+#                 user.save()
+#                 login(request, user)
+#                 return redirect('home')
+#             except IntegrityError:
+#                 return render(request, 'create_admin.html', {'form': UserCreationForm(), 'error': 'Username already exists.'})
+#         else:
+#             return render(request, 'create_admin.html', {'form': UserCreationForm(), 'error': 'Password did not match'})
+
 
 @login_required
 def logoutuser(request):
